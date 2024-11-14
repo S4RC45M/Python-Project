90a43c6c39106386d1ba623466b169a03c031a29
RosieThyme <137115930+RosieThyme@users.noreply.github.com>
2024-11-14 16:57:47 -0500

    Update Visuals.py

--- Visuals.py
+++ Visuals.py
@@ -1,6 +1,9 @@
 #import module tkinter with the nickname tk
 import tkinter as tk
-
+#button actions
+#allows the user to enter data
+def enter():
+    return None
 #constants for color and fonts
 BACKGROUNDCOLOR = "#FEF6C9"
 FONTNAME="Courier"
@@ -11,6 +14,8 @@ window.minsize(800,600)
 window.config(bg=BACKGROUNDCOLOR)
 window.rowconfigure([0,1,2,3,4],minsize=100,weight=1)
 window.columnconfigure([0,1,2,3,4],minsize=100,weight=1)
-
+#it says hello :)
+titleHello = tk.Label(text = "Hello")
+titleHello.grid(column=2 ,row=2)
 #keeps the window open
 window.mainloop()
