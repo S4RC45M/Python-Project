a69576f1b930611dde8461af3e0af46f490892ec
RosieThyme <137115930+RosieThyme@users.noreply.github.com>
2024-11-14 17:23:36 -0500

    Create spaceobject Dictionary

---
+++ DistanceGame/CometDict.py
@@ -0,0 +1,22 @@
+def createspaceobject(name,orbityears):
+    return spaceobjects.append({"name": name, "orbityears": orbityears})
+
+
+spaceobjects = [
+    {"name": "Hailey's Comet",
+     "orbityears": 76 ,
+       },
+    {"name": "Jupiter",
+        "orbityears": 12,     
+       },
+    {"name": "Eris",
+     "orbityears": 556,
+       },
+    {"name": "Jupiter",
+        "orbityears": 12,     
+       },
+    {
+    
+    }
+
+]
