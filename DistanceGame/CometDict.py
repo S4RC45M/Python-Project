c175b9eb1d9d3e7308106a65fd45403a82a3b166
RosieThyme <137115930+RosieThyme@users.noreply.github.com>
2024-11-16 15:53:52 -0500

    modifed DistanceGame

--- DistanceGame/CometDict.py
+++ DistanceGame/CometDict.py
@@ -1,4 +1,7 @@
+import math
 def createspaceobject(name,orbityears):
+    name = name.capitalize()
+    orbityears = round(orbityears)
     return spaceobjects.append({"name": name, "orbityears": orbityears})
 
 
@@ -17,6 +20,11 @@ spaceobjects = [
        },
     {"name": "Neptune",
         "orbityears": 165,     
+    },
+    {
+        "name":"Swift-Tuttle",
+        "orbityears": 133
     }
 
 ]
+createspaceobject("Borrelly",6.84)
--- DistanceGame/main.py
+++ DistanceGame/main.py
@@ -1,3 +1,16 @@
 import CometDict
-class highorlow(self):
-    return None
+import random as r
+data = CometDict.spaceobjects
+ongoing = True
+spaceobjlen = len(data)
+# randnum1 = r.randint(0,(spaceobjlen-1))
+# randnum2 = r.randint(0,(spaceobjlen-1))
+# print(data, spaceobjlen, randnum1,randnum2)
+print("Guess which one is higher!\n")
+randnum1 = r.randint(0,(spaceobjlen-1))
+randnum2 = r.randint(0,(spaceobjlen-1))
+input(f"which object is further away from the sun, {data[randnum1]["name"]} or {data[randnum2]["name"]}?\n")
+# while ongoing:
+#     randnum1 = r.randint(0,(spaceobjlen-1))
+#     randnum2 = r.randint(0,(spaceobjlen-1))
+#     input(f"which object is further away from the sun, {[randnum1]["name"]} or{[randnum1]["name"]}?\n")
