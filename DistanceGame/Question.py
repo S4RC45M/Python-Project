32812c4ea15a70411553b1cd7df20edfbdbc9887
RosieThyme <137115930+RosieThyme@users.noreply.github.com>
2024-11-19 17:47:09 -0500

    changes made to Question.py

--- DistanceGame/Question.py
+++ DistanceGame/Question.py
@@ -1,8 +1,9 @@
 class Question:
-    def __init__(self, object, answer):
-        self.object = object
+    def __init__(self, name, answer):
+        self.name = name
         self.answer = answer
+    
 
-Q1 = Question("tomato",6)
+# Q1 = Question("tomato",6)
 
-print(f"{Q1.object}")
+# print(f"{Q1.object}")
