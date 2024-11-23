4c1d143c70826288641dac1e9c5939813ecc4594
RosieThyme <137115930+RosieThyme@users.noreply.github.com>
2024-11-23 18:18:53 -0500

    Update CometDict.py

--- COP1500Project/CometDict.py
+++ COP1500Project/CometDict.py
@@ -9,21 +9,41 @@ spaceobjects = [
      "orbityears": 76 ,
        },
     {"name": "Jupiter",
-        "orbityears": 12,
+        "orbityears": 12,     
        },
     {"name": "Eris",
      "orbityears": 556,
        },
     {"name": "Vesta",
-        "orbityears": 4,
+        "orbityears": 4,     
        },
     {"name": "Neptune",
-        "orbityears": 165,
+        "orbityears": 165,     
     },
     {
         "name":"Swift-Tuttle",
         "orbityears": 133
+    },
+        {
+        "name":"Quaoar",
+        "orbityears": 289
+    },
+        {
+        "name":"Gonggong",
+        "orbityears": 554
+    },
+        {
+        "name":"Tempel 1",
+        "orbityears": 6
+    },
+        {
+        "name":"Mr.Spock",
+        "orbityears": 5
     }
 
 ]
 createspaceobject("Borrelly",6.84)
+createspaceobject("Orcus",245)
+createspaceobject("Haumea",283.77)
+createspaceobject("salacia", 276.52)
+createspaceobject("Sedna", 11400)
